cb005bd9e8a85a3ea5df7f64ebb689622b5928b9
gabrielmm1234 <gabrielmesquita@cjr.org.br>
2016-03-19 03:40:05 -0300

    angular templates from html to asset pipeline e js folder struct

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,4 +1,4 @@
-angular.module('flapperNews', ['ui.router'])
+angular.module('flapperNews', ['ui.router','templates'])
   .config([
 '$stateProvider',
 '$urlRouterProvider',
@@ -7,13 +7,13 @@ function($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state('home', {
       url: '/home',
-      templateUrl: '/home.html',
+      templateUrl: 'home/_home.html',
       controller: 'MainCtrl'
     });
   $stateProvider
     .state('posts', {
       url: '/posts/{id}',
-      templateUrl: '/posts.html',
+      templateUrl: 'posts/_posts.html',
       controller: 'PostsCtrl'
   });
 
